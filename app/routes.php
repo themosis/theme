a36ed99009db4576771fa94c4363f8d17fca2e85
Julien Lambé <julien@jlambe.be>
2014-04-22 16:02:07 +0200

    Fix some typos in the routes.php file.

--- app/routes.php
+++ app/routes.php
@@ -1,20 +1,15 @@
 <?php defined('DS') or die('No direct script access.');
 
 /*
-* Define your routes and which views to display
-* depending of the query.
-*
-* Based on Wordpress conditional tags from the Wordpress Codex
-* http://codex.wordpress.org/Conditional_Tags 
-*
-*/
+ * Define your routes and which views to display
+ * depending of the query.
+ *
+ * Based on WordPress conditional tags from the WordPress Codex
+ * http://codex.wordpress.org/Conditional_Tags
+ *
+ */
 Route::is('home', function(){
 
-    global $post;
+    return "Congratulations! The Themosis framework is running!";
 
-	return View::make('home', array(
-        'name'      => 'Julien',
-        'article'      => $post
-    ));
-
-});
+};
