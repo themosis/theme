44d884a1623849e1c2ddd7f44abb29f322fb5237
Julien Lambé <julien@jlambe.be>
2014-12-06 11:10:42 +0100

    Update welcome view.

--- app/views/welcome.scout.php
+++ app/views/welcome.scout.php
@@ -8,8 +8,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title><?php wp_title(); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400' rel='stylesheet' type='text/css'>
     <style type="text/css">
+        /* latin */
+        @font-face {
+            font-family: 'Open Sans';
+            font-style: normal;
+            font-weight: 400;
+            src: local('Open Sans'), local('OpenSans'), url(http://fonts.gstatic.com/s/opensans/v10/cJZKeOuBrn4kERxqtaUH3ZBw1xU1rKptJj_0jans920.woff2) format('woff2');
+            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215, U+E0FF, U+EFFD, U+F000;
+        }
+
         .clearfix:before,
         .clearfix:after {
             content: " "; /* 1 */
