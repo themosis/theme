20f61fd81ace2a3bc9ac5165cb1e9c2742677f9e
Julien Lambé <julien@jlambe.be>
2015-06-29 21:50:42 +0200

    Test basic textfield for users. Only display.

--- resources/admin/user.php
+++ resources/admin/user.php
@@ -1,9 +1,19 @@
 <?php
 
-//tp(User::misc());
 
+/*
 Action::add('user_new_form', 'UserController@display');
 // When editing another user profile.
 Action::add('edit_user_profile', 'UserController@display');
 // When editing its own profile.
 Action::add('show_user_profile', 'UserController@display');
+*/
+
+//User::addSections();
+User::addFields([
+    Field::text('facebook', ['title' => 'Facebook profile'])
+]);
+
+User::addFields([
+    Field::text('twitter')
+]);
