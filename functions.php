89a60145ef3a9089c7a97bf4c6aa0309e5d04f25
Julien Lambé <julien@themosis.com>
2018-08-28 17:38:23 +0200

    Add theme translations.

--- functions.php
+++ functions.php
@@ -2,4 +2,29 @@
 
 use Themosis\Core\Application;
 
+/*
+|--------------------------------------------------------------------------
+| Bootstrap Theme
+|--------------------------------------------------------------------------
+|
+| We bootstrap the theme. The following code is loading your theme
+| configuration files and register theme images sizes, menus, sidebars,
+| theme support features and templates.
+*/
 (Application::getInstance())->loadTheme(__DIR__, 'config');
+
+/*
+|--------------------------------------------------------------------------
+| Theme i18n | l10n
+|--------------------------------------------------------------------------
+|
+| Registers the "languages" directory for storing the theme translations.
+| The "THEME_TD" constant is defined during bootstrap and its value is
+| set based on the "style.css" [Text Domain] property located into
+| the file header.
+|
+*/
+load_theme_textdomain(
+    THEME_TD,
+    get_template_directory().'/languages'
+);
